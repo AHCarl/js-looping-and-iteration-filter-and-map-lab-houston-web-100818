73522e9d16a8ffebc92b02c425ec4f902b233c85
Alexander Carlin <ahcarlin@gmail.com>
2018-11-16 22:54:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ function driverNamesWithRevenueOver(driver, revenue) {
   drivers = driver.filter( d => d.revenue > revenue )
   return drivers.map( d => d.name)
 }
+
+function exactMatch(driver, obj) {
+  return driver.filter( d => d)
+}
